5c1b8a1d18571d076fc4d77f16992b72dc065b2d
bayuyuhartono <bayuyuhartono@gmail.com>
2026-02-24 09:02:01 +0700

    fix: monthly twin

--- Modules/Akap/app/Http/Controllers/AkapMonthlyTwinController.php
+++ Modules/Akap/app/Http/Controllers/AkapMonthlyTwinController.php
@@ -22,15 +22,15 @@ class AkapMonthlyTwinController extends Controller
         $year = ($request->has('year')) ? $request->input('year') : date('Y');
         $trip = $request->input('trip');
 
-        if (env('AKAP_MONTHLY_REPORT_CACHE_ENABLED', false)) {
-            $cacheKey = "akap_monthly_repr1_{$year}_{$month}_{$trip}";
+        // if (env('AKAP_MONTHLY_REPORT_CACHE_ENABLED', false)) {
+        //     $cacheKey = "akap_monthly_repr1_{$year}_{$month}_{$trip}";
 
-            $data = Cache::remember($cacheKey, 60 * 60, function () use ($request, $month, $year, $trip) {
-                return $this->getReportData($request, $month, $year, $trip);
-            });
-        } else {
+        //     $data = Cache::remember($cacheKey, 60 * 60, function () use ($request, $month, $year, $trip) {
+        //         return $this->getReportData($request, $month, $year, $trip);
+        //     });
+        // } else {
             $data = $this->getReportData($request, $month, $year, $trip);
-        }
+        // }
 
         echo json_encode($data);
         return;

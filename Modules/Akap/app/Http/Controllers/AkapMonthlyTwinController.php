e03ddd1727f1950862fe241cbb94a5db146bc954
bayuyuhartono <bayuyuhartono@gmail.com>
2026-02-24 10:14:27 +0700

    fix: monthly twin

--- Modules/Akap/app/Http/Controllers/AkapMonthlyTwinController.php
+++ Modules/Akap/app/Http/Controllers/AkapMonthlyTwinController.php
@@ -1070,7 +1070,7 @@ class AkapMonthlyTwinController extends Controller
 
         // ADD TEMPORARILY OPEN BUSES (status=0 with trip_assign_temporary records)
         $tempOnClassInfo = Akap::getTemporaryOnClassInfo($param);
-        $tempOnGrouped = collect($tempOnClassInfo)->groupBy('type');
+        $tempOnGrouped = collect($tempOnClassInfo)->groupBy('tras_id');
         foreach ($tempOnGrouped as $trasId => $group) {
             $entry = clone $group->first();
             $entry->days_active = 0;
